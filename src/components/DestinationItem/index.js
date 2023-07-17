387d67813ae4d9836f47c03b1348aa7a281063ac
leelambica <leelambicagudimetla@gmail.com>
2023-07-17 20:34:16 +0000

    destination

--- src/components/DestinationItem/index.js
+++ src/components/DestinationItem/index.js
@@ -1,38 +1,15 @@
 import './index.css'
-import {Component} from 'react'
-import DestinationSearch from './components/DestinationSearch'
-import './App.js'
 
-const initialdestinationsList = []
+const DestinationItem = props => {
+  const {destinationsList} = props
+  const {imgUrl, name} = destinationsList
 
-class DestinationItem extends Component {
-  state = {
-    searchInput: '',
-    destinationsList: initialdestinationsList,
-  }
-
-  onChangeSearchInput = event => {
-    this.setState({searchInput: event.target.value})
-  }
-
-  render() {
-    const {searchInput, destinationsList} = this.state
-    return (
-      <div className="main-container">
-        <h1 className="heading">Destination Search</h1>
-        <div>
-          <input type="search" placeholder="Search" value={searchInput} />
-          <img
-            src="https://assets.ccbp.in/frontend/react-js/destinations-search-icon-img.png"
-            alt="search icon"
-          />
-        </div>
-
-        <ul className="list-container">
-          <DestinationSearch />
-        </ul>
-      </div>
-    )
-  }
+  return (
+    <li className="user-card-container">
+      <img src={imgUrl} className="profile-pic" value={name} alt={name} />
+      <p className="user-name"> {name} </p>
+    </li>
+  )
 }
+
 export default DestinationItem
--- src/components/DestinationSearch/index.js
+++ src/components/DestinationSearch/index.js
@@ -1,15 +1,63 @@
 import './index.css'
-import './App.js'
+import {Component} from 'react'
+import DestinationItem from '../DestinationItem'
 
-const DestinationSearch = props => {
-  const {destinationsList} = props
-  const {imgUrl, name, id} = destinationsList
+class DestinationSearch extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {destinationsList: props.destinationsList, searchInput: ''}
+  }
 
-  return (
-    <li className="user-card-container">
-      <img src={imgUrl} className="profile-pic" alt="destinationsList" />
-      <h1 className="user-name"> {name} </h1>
-    </li>
-  )
+  fu = event => {
+    const {searchInput} = this.state
+    const {destinationsList} = this.props
+    console.log(event.target.value)
+
+    const listOfNew = destinationsList.filter(m =>
+      m.name.toLowerCase().includes(searchInput.toLowerCase()),
+    )
+    this.setState({
+      destinationsList: listOfNew,
+      searchInput: event.target.value,
+    })
+  }
+
+  f1 = finding => {
+    const {destinationsList} = this.state
+
+    const list = destinationsList.filter(each => each.id !== finding)
+
+    this.setState({destinationsList: list})
+  }
+
+  render() {
+    const {searchInput, destinationsList} = this.state
+    const searchResults = destinationsList.filter(each =>
+      each.name.toLowerCase().includes(searchInput.toLowerCase()),
+    )
+    return (
+      <div className="main-container">
+        <h1 className="heading">Destination Search</h1>
+        <div>
+          <input
+            type="search"
+            placeholder="Search"
+            onChange={this.fu}
+            value={searchInput}
+          />
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/destinations-search-icon-img.png"
+            alt="search icon"
+          />
+        </div>
+
+        <ul className="list-container">
+          {searchResults.map(each => (
+            <DestinationItem destinationsList={each} key={each.id} />
+          ))}
+        </ul>
+      </div>
+    )
+  }
 }
 export default DestinationSearch
